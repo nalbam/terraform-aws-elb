4bfa034840f139e2b36bca21c896f2ff92f10bf2
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2018-03-09 17:13:50 +0900

    dns

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-//output "elb_name" {
-//  value = "${aws_elb.default.name}"
-//}
+output "elb_name" {
+  value = "${aws_elb.default.name}"
+}
 
-//output "elb_dns_name" {
-//  value = "${aws_elb.default.dns_name}"
-//}
+output "elb_dns_name" {
+  value = "${aws_elb.default.dns_name}"
+}
